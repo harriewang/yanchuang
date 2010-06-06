a8501cbbbeea3957a2dbfda932d59716a032a5f5
luffy <luffy@zhoujialiang.(none)>
2010-06-06 19:26:51 +0800

    delete redundant functions

--- apps/local/studioconfigs/context_processors.py
+++ apps/local/studioconfigs/context_processors.py
@@ -1,19 +1,6 @@
 import settings
 from models import StudioConfig, StudioLink
 
-def i18n(request):
-    from django.utils import translation
-    
-    context_extras = {}
-    context_extras['LANGUAGES'] = settings.LANGUAGES
-    context_extras['LANGUAGE_CODE'] = translation.get_language()
-    context_extras['LANGUAGE_CODE_URL'] = translation.get_language().replace('-', '_')
-    #context_extras['LANGUAGE_BIDI'] = translation.get_language_bidi()
-    #context_extras['LANGUAGE_SMALL'] = context_extras['LANGUAGE_CODE'].split('-')[0]
-    
-    return context_extras
-
-
 def studio_base(request):
     """Put studio base information and friend links into generic Context"""
     
@@ -28,9 +15,3 @@ def studio_base(request):
         context_studio_base['STUDIO_LINKS'] = None
 
     return context_studio_base
-
-def media(request):
-	context_media = {}
-	context_media['MEDIA_URL'] = settings.MEDIA_URL
-	
-	return context_media
